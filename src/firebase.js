17f8e3623de8f7ac564a48133ef4a96ef3cbd44a
bea <chamoischamois@protonmail.com>
2022-05-22 22:33:47 +0800

    Added function to retrieve image from firestore

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,22 @@
 import { initializeApp } from "firebase/app";
-import { getConfig } from "./getConfig";
-import { getStorage } from "firebase/storage";
+import getConfig from "./getConfig";
+import { getStorage, ref, getDownloadURL } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const config = getConfig();
 const app = initializeApp(config);
 const storage = getStorage(app);
 const db = getFirestore(app);
+
+function retrieveImage() {
+    getDownloadURL(ref(storage, 'gs://hidden-object-game-3372e.appspot.com/930px-Last_Judgement_(Michelangelo).jpg'))
+  .then((url) => {
+    const img = document.getElementById('loaded-image');
+    img.setAttribute('src', url);
+  })
+  .catch((error) => {
+    throw error;
+  });
+}
+
+export { retrieveImage };

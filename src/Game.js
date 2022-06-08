1dfe5b979b0dd6cd2445a4ba0a23b18f30e7e6d8
bea <chamoischamois@protonmail.com>
2022-06-08 14:38:51 +0800

    Added function to create targeting box on click

--- src/Game.js
+++ src/Game.js
@@ -1,15 +1,44 @@
 import { retrieveImage } from './firebase';
+import { createTargetBox, displaySelectionMenu } from './gameplay';
 
 function Game () {
+    function handleBegin (event) {
+        event.target.disabled = 'true';
+        const imageContainer = document.querySelector('.image-container');
+        imageContainer.style.pointerEvents = 'all';
+    }
+
+    function handleClick(event) {
+        const clickTarget = event.target;
+        if (clickTarget.tagName === 'IMG') {
+        const xCoord = event.pageX;
+        const yCoord = event.pageY;
+        createTargetBox(xCoord, yCoord);
+        //selection box popup
+        }
+    }
+
     retrieveImage();
+
     return (
         <main>
             <h1>Game</h1>
-            <button>Begin!</button>
-            <div className="people-container">
+            <button onClick={handleBegin}>Begin!</button>
+            <div className="people-list">
                 Look for them:
+                <div className='people-container'>
+                    <div>
+                        Michelangelo
+                    </div>
+                    <div>
+                        St Sebastian
+                    </div>
+                    <div>
+                        <a href='https://en.wikipedia.org/wiki/Biagio_da_Cesena'>Minos</a>
+                    </div>
+                </div>
             </div>
-            <div className="image-container">       
+            <div className="image-container" onClick={handleClick}>       
                 <img id="loaded-image" alt="The Last Judgment by Michalangelo" /> 
             </div>
         </main>
---
+++ src/gameplay.js
@@ -0,0 +1,23 @@
+function createTargetBox (x, y) {
+    const prevTargetBox = document.querySelector('.target-box');
+    if (prevTargetBox !== null) {
+    prevTargetBox.remove();
+    };
+    const targetBox = document.createElement('div');
+    const image = document.getElementById('loaded-image');
+    const imageCoords = image.getBoundingClientRect();
+    targetBox.style.left = x - imageCoords.x - (22) + 'px';
+    targetBox.style.top = y - 200 - (32) + 'px';
+    targetBox.classList.add('target-box');
+    const imageContainer = document.querySelector('.image-container');
+    imageContainer.appendChild(targetBox);
+}
+
+function displaySelectionMenu (x, y) {
+    
+}
+
+function checkCoordinates (x , y) {
+}
+
+export { createTargetBox, displaySelectionMenu, checkCoordinates };

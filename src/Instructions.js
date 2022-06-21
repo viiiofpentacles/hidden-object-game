fc8f7e88bf6fff4b443f0a99be1fd151175761ef
Bea <chamoischamois@protonmail.com>
2022-06-21 15:53:21 +0800

    Added more text to clarify instructions

--- src/Instructions.js
+++ src/Instructions.js
@@ -3,7 +3,13 @@ function Instructions () {
         <main>
             <h1>Instructions</h1>
             <p>
-                Click on the 'Play!' link in the navigation menu to be directed to the game. Click on 'Begin!' to start the game and timer. Click on the area in the image where you think the person you are looking for is located. The game ends once you have found all of them!
+                Click on the 'Play!' link in the navigation menu to be directed to the game. Clicking on the 'Begin!' button starts the game and timer.
+            </p>
+            <p>
+                Click on the area in the image where you think the person you are looking for is located. The game ends once you have found all of them!
+            </p>
+            <p>
+                You can click on the names for clues (Opens a new page to external site).
             </p>
         </main>
     )
